65cea6630fee6e67193fee10c51c19f87477c8e1
Lars Roesicke <lars.roesicke@e3dc.com>
2025-12-13 16:29:45 +0100

    Add hint to help if no valid option was passed to script

--- app/file-janitor.sh
+++ app/file-janitor.sh
@@ -2,6 +2,8 @@
 echo 'File Janitor, 2025'
 echo 'Powered by Bash'
 
-if [ $1 = "help" ]; then
+if [ "$1" = "help" ]; then
    cat ./file-janitor-help.txt
+else
+  echo "Type $0 help to see available options"
 fi
