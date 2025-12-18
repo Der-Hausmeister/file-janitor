745cb3e7d1c276924323bdb43ad0c78ad27a9971
Lars Roesicke <lars.roesicke@e3dc.com>
2025-12-18 22:11:38 +0100

    Add more error handling to report function

--- app/file-janitor.sh
+++ app/file-janitor.sh
@@ -12,30 +12,42 @@ list() {
       return 0
   fi
 
-  local dir=$1;
-  if [ -z "$dir" ]; then
+  DIR=$1;
+  if [ -z "$DIR" ]; then
     echo -e "Listing files in the current directory\n"
-    dir=.
+    DIR=.
   else
     echo -e "Listing files in $1\n"
   fi
-  ## Okay, this is super complicated. This is the same as 'ls -A1 "$dir"' :-)
-  ls -lA "$dir" | tail -n +2 | tr -s '[:space:]' | cut -d ' ' -f 9
+  ## Okay, this is super complicated. This is the same as 'ls -A1 "$DIR"' :-)
+  ls -lA "$DIR" | tail -n +2 | tr -s '[:space:]' | cut -d ' ' -f 9
 }
 
 printFilesAndBytes() {
   FILES_COUNT=$(find "$1" -maxdepth 1 -name "*.$2" | wc -w)
   FILES_SIZE=$(find "$1" -maxdepth 1 -name "*.$2" -exec wc -c {} \+ | tail -n 1 | cut -d " " -f 1)
+  if [ -z "$FILES_SIZE" ]; then
+      FILES_SIZE=0
+  fi
   echo "$FILES_COUNT $2 file(s), with total size of $FILES_SIZE bytes"
 }
 
 report() {
-  DIR=".";
-  if [ -n "$1" ]; then
-    DIR=$1
-    echo "$1 contains:"
-  else
+  if [ -n "$1" ] && [ ! -e "$1" ]; then
+      echo -e "$1 is not found"
+      return 0
+  fi
+  if [ -n "$1" ] && [ ! -d "$1" ]; then
+      echo -e "$1 is not a directory"
+      return 0
+  fi
+
+  DIR=$1
+  if [ -z "$1" ]; then
+    DIR=.
     echo "The current directory contains:"
+  else
+    echo "$1 contains:"
   fi
 
   printFilesAndBytes $DIR "tmp"
