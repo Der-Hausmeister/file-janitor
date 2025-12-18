de2c5cac8e37222527b71670894ac9a05c4ea5b1
Lars Roesicke <lars.roesicke@e3dc.com>
2025-12-18 21:48:58 +0100

    Implement report option for janitor. Add test files
    I added a simple report function that will display the amount of certain files types in a directory

--- app/file-janitor.sh
+++ app/file-janitor.sh
@@ -20,13 +20,35 @@ list() {
     echo -e "Listing files in $1\n"
   fi
   ## Okay, this is super complicated. This is the same as 'ls -A1 "$dir"' :-)
-  ls -lA "$dir" | tail -n +4 | tr -s '[:space:]' | cut -d ' ' -f 9
+  ls -lA "$dir" | tail -n +2 | tr -s '[:space:]' | cut -d ' ' -f 9
+}
+
+printFilesAndBytes() {
+  FILES_COUNT=$(find "$1" -maxdepth 1 -name "*.$2" | wc -w)
+  FILES_SIZE=$(find "$1" -maxdepth 1 -name "*.$2" -exec wc -c {} \+ | tail -n 1 | cut -d " " -f 1)
+  echo "$FILES_COUNT $2 file(s), with total size of $FILES_SIZE bytes"
+}
+
+report() {
+  DIR=".";
+  if [ -n "$1" ]; then
+    DIR=$1
+    echo "$1 contains:"
+  else
+    echo "The current directory contains:"
+  fi
+
+  printFilesAndBytes $DIR "tmp"
+  printFilesAndBytes $DIR "log"
+  printFilesAndBytes $DIR "py"
 }
 
 if [ "$1" = "help" ]; then
   cat ./file-janitor-help.txt
 elif [ "$1" = "list" ]; then
   list "$2"
+elif [ "$1" = "report" ]; then
+  report "$2"
 else
   echo "Type $0 help to see available options"
 fi
