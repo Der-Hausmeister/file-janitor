6df40507045d367594fb91a5146b116a09beedb1
Lars Roesicke <lars.roesicke@e3dc.com>
2025-12-23 11:20:09 +0100

    Add command that cleans up certain file types

--- app/file-janitor.sh
+++ app/file-janitor.sh
@@ -2,6 +2,8 @@
 echo 'File Janitor, 2025'
 echo -e 'Powered by Bash\n'
 
+FILES_COUNT=0
+
 is_validate_directory() {
     if [ -n "$1" ] && [ ! -e "$1" ]; then
         echo -e "$1 is not found"
@@ -34,8 +36,12 @@ list() {
   ls -lA "$DIR" | tail -n +2 | tr -s '[:space:]' | cut -d ' ' -f 9
 }
 
+countFiles() {
+  FILES_COUNT=$(find "$1" -maxdepth 1 -type f -name "*.$2" | wc -w)
+}
+
 printFilesAndBytes() {
-  FILES_COUNT=$(find "$1" -maxdepth 1 -name "*.$2" | wc -w)
+  countFiles "$1" "$2"
   ## Nice is also: "find  -maxdepth 1 -name '*.ps' -type f -printf "%s"",
   # but this also does not help with the empty result
   FILES_SIZE=$(find "$1" -maxdepth 1 -name "*.$2" -exec wc -c {} \+ | tail -n 1 | cut -d " " -f 1)
@@ -63,12 +69,52 @@ report() {
   printFilesAndBytes $DIR "py"
 }
 
+clean() {
+  if ! is_validate_directory "$1"; then
+        return 1
+  fi
+
+  DIR=$1
+  SUCCESS_MSG="Clean up of $DIR is complete!"
+  if [ -z "$1" ]; then
+    ## I think one can define a default to a var with something like this DIR="${2:-"."}". Seems also reasonable to me
+    DIR=.
+    echo "Cleaning the current directory..."
+    SUCCESS_MSG="Clean up of the current directory is complete!"
+  else
+    echo "Cleaning $1..."
+  fi
+
+  echo -n "Deleting old log files...  "
+  FILES_COUNT=$(find "$DIR" -maxdepth 1 -type f -mtime +3  -name "*.log" | wc -w)
+  find "$DIR" -type f -mtime +3 -name "*.log" -exec rm {} \;
+  echo "done! $FILES_COUNT files have been deleted"
+
+  echo -n "Deleting temporary files...  "
+  countFiles "$DIR" "tmp"
+  ## Also good idea it to use -print option that prints the found files at the end  even with the exec option in place.
+  ## we can then pipe into wc as we do in countFiles
+  find "$DIR" -maxdepth 1 -type f -name "*.tmp" -exec rm {} \;
+  echo "done! $FILES_COUNT files have been deleted"
+
+  echo -n "Moving python files...  "
+  countFiles "$DIR" "py"
+  if [ "$FILES_COUNT" -ne 0 ]; then
+     mkdir "$DIR/python_scripts"
+     find "$DIR" -maxdepth 1 -type f -name "*.py" -exec mv {} -t "$DIR/python_scripts" \;
+  fi
+  echo "done! $FILES_COUNT files have been moved"
+  echo -e "\n$SUCCESS_MSG"
+}
+
 if [ "$1" = "help" ]; then
   cat ./file-janitor-help.txt
 elif [ "$1" = "list" ]; then
   list "$2"
 elif [ "$1" = "report" ]; then
   report "$2"
+elif [ "$1" = "clean" ]; then
+  clean "$2"
 else
   echo "Type $0 help to see available options"
 fi
